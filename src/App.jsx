3d11122064c94ce738f0b5e5d75f6683ddbe616a
Dharani Govardhan <palerugopi00@gmail.com>
2026-03-08 15:38:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import DownloadAppBanner from "./components/DownloadAppBanner";
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
